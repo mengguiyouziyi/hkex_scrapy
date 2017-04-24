1e76c2b590203b224cd0bd64e8519033cdea250f
mengguiyouziyi <775618369@qq.com>
2017-04-24 16:29:45 +0800

    4.1

--- hkex/settings.py
+++ hkex/settings.py
@@ -14,14 +14,14 @@ SPIDER_MODULES = ['hkex.spiders']
 NEWSPIDER_MODULE = 'hkex.spiders'
 
 DOWNLOADER_MIDDLEWARES = {
-    'hkex.middlewares.ProxyMiddleware': 100,#代理中间件
+    # 'hkex.middlewares.ProxyMiddleware': 100,#代理中间件
     'hkex.middlewares.RotateUserAgentMiddleware': 200,#请求头中间件
     # 'crawler.middlewares.JavaScriptMiddleware': 543,  # 键为中间件类的路径，值为中间件的顺序
     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,  # 禁止内置的中间件
 }
 
 ITEM_PIPELINES = {
-    'hkex.pipelines.MongodbPipeline': 800,
+    # 'hkex.pipelines.MongodbPipeline': 800,
 }
 
 #禁用cookies
@@ -35,7 +35,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 2  # 间隔时间,两次下载的间隔
+DOWNLOAD_DELAY = 1  # 间隔时间,两次下载的间隔
 # RANDOMIZE_DOWNLOAD_DELAY = True  # 开启随机延迟
 
 USER_AGENT_CHOICES = [
@@ -46,7 +46,7 @@ USER_AGENT_CHOICES = [
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11) AppleWebKit/601.1.56 (KHTML, like Gecko) Version/9.0 Safari/601.1.56',
     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/601.2.7 (KHTML, like Gecko) Version/9.0.1 Safari/601.2.7',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
 ]
 
 LOG_STDOUT = True
