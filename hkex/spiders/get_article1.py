a0ad6bd556a5c8c488c1bbac17febc4152026b4c
mengguiyouziyi <775618369@qq.com>
2017-04-27 12:43:36 +0800

    4.5

--- hkex/spiders/get_article1.py
+++ hkex/spiders/get_article1.py
@@ -16,7 +16,7 @@ class HkexSpider(CrawlSpider):
             hkex_cns = file.readlines()
         for index, hkex_cn in enumerate(hkex_cns):
             count = index + 1
-            if count < 109227:
+            if count < 63662:
                 continue
             request = scrapy.Request('http://www.hkexnews.hk'+hkex_cn.strip(), callback=self.parse_item)
             request.meta['count'] = count
